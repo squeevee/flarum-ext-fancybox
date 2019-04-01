2fcf88a3326400306bc0e4ecd9220bc67a206e4c
Ellie Hawk <squeevee@yahoo.com>
2019-04-01 16:51:47 -0600

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# FancyBox Extension for Flarum
+
+Image formatting and lightbox modal using FancyBox.
+
+For more information about FancyBox, see: https://fancyapps.com/fancybox
+
+## Features
+
+ - Clicking on an image in a post launches the FancyBox modal. This allows users
+to zoom in on the image, pan around, and navigate among multiple images on the
+same post as a gallery.
+- Images are displayed with formatting to normalize sizes and layout.
+- If the author of the post has wrapped the image in a link, it will be shown
+as an external link. (Clicking on the image will not launch the FancyBox modal
+in this case.)
+
+Note: this extension does not generate thumbnails. The sorage location of
+images, and the file size / bandwidth usage are not affected.
+
+## Installation
+
+This repository will soon be on Packagist, meaning that it will be able to be
+installed via a `composer` command. Stay tuned.
+
+## License
+
+This extension is licensed under GPLv3. This extension is not affiliated with
+or endorsed by the makers of FancyBox.
---
+++ js/src/forum/index.js
@@ -0,0 +1,94 @@
+/*  
+ *  FancyBox Extension for Flarum
+ *  Copyright (C) 2019 Eleanor Hawk
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ */
+
+import "@fancyapps/fancybox";
+
+import { extend } from 'flarum/extend';
+import Post from 'flarum/components/Post';
+import ModalManager from 'flarum/components/ModalManager';
+
+function categorizeImages(element) {
+  $(element).find('p > img:not([class])').each((i, e) => {
+    if ($(e).parent().contents().length === 1)
+      $(e).addClass('block-image');
+    else
+      $(e).addClass('inline-image');
+  });
+  $(element).find('p a:not(.block-image-link,.inline-image-link) > img:not([class])').each((i, e) => {
+    let link = $(e).parent();
+    if (link.contents().length === 1
+      && link.parent().contents().length === 1) {
+      $(e).addClass('block-image');
+      link.addClass('block-image-link');
+    } else {
+      $(e).addClass('inline-image');
+      link.addClass('inline-image-link');
+    }
+    let iconWrapper = document.createElement('div');
+    iconWrapper.className = 'extlink-badge';
+    let icon = document.createElement('i');
+    icon.className = 'fas fa-external-link-alt';
+    iconWrapper.appendChild(icon);
+    link.append(iconWrapper);
+  });
+}
+
+app.initializers.add('fancybox', app => {
+  $.fancybox.defaults.toolbar = false;
+  $.fancybox.defaults.lang = app.translator.locale;
+  $.fancybox.defaults.i18n[app.translator.locale] = {
+    NEXT: app.translator.trans('fancybox.forum.next'),
+    PREV: app.translator.trans('fancybox.forum.prev'),
+    ERROR: app.translator.trans('fancybox.forum.error')
+  }
+
+  extend(Post.prototype, 'config', function(x, isInitialized, context) {
+    categorizeImages(this.element);
+    if (!this.isEditing() && !('fancybox_gallery' in this)) {
+      let images = $(this.element).find('img.inline-image,img.block-image').not('a *');
+      let gallery = images.map((i, e) => {
+        return {
+          src: e.getAttribute('src'),
+          type: 'image'
+        }
+      });
+      this.fancybox_gallery = gallery.length ? gallery : false;
+      if (this.fancybox_gallery) {
+        images.each((i, e) => {
+          let index = i;
+          e.style.cursor = 'pointer';
+          $(e).off('click.fancybox');
+          $(e).on('click.fancybox', (event) => {
+            $.fancybox.open(this.fancybox_gallery, {}, index);
+          });
+        });
+      }
+    } else if (this.isEditing() && 'fancybox_gallery' in this) {
+      delete this.fancybox_gallery;
+    }
+  });
+
+  extend(ModalManager.prototype, 'show', function (x) {
+    $.fancybox.close();
+  })
+
+  if (s9e && s9e.TextFormatter) {
+    extend(s9e.TextFormatter, 'preview', function(x, preview, element) {
+      if (element.matches('.Post *'))
+        categorizeImages(element);
+    });
+  }
+});
+
